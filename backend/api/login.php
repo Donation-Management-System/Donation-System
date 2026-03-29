618a3aadf816c7d6c2089aff4b5598445b767b12
Md. Minhaj <md.minhaj.cse@ulab.edu.bd>
2026-03-30 02:36:59 +0600

    Update login.php

--- backend/api/login.php
+++ backend/api/login.php
@@ -3,46 +3,47 @@ session_start();
 header("Content-Type: application/json");
 include("../config/db.php");
 
-// Get JSON
 $data = json_decode(file_get_contents("php://input"), true);
 
-// Check data
 if(!$data){
-    echo json_encode(["error"=>"No data received"]);
+    echo json_encode(["status"=>"error","message"=>"No data received"]);
     exit;
 }
 
 $email = $conn->real_escape_string($data['email']);
 $password = $data['password'];
 
-// Validation
 if(empty($email) || empty($password)){
-    echo json_encode(["error"=>"Email and password required"]);
+    echo json_encode(["status"=>"error","message"=>"Email and password required"]);
     exit;
 }
 
-// Find user
 $result = $conn->query("SELECT * FROM users WHERE email='$email'");
 
 if(!$result || $result->num_rows == 0){
-    echo json_encode(["error"=>"User not found"]);
+    echo json_encode(["status"=>"error","message"=>"User not found"]);
     exit;
 }
 
 $user = $result->fetch_assoc();
 
-// Check password
+
 if($password == $user['password']){
+
+    $_SESSION['user_id'] = $user['id'];
+
     echo json_encode([
+        "status"=>"success",
         "message"=>"Login successful",
         "user_id"=>$user['id'],
         "name"=>$user['name'],
         "role"=>$user['role']
     ]);
+
 }else{
     echo json_encode([
-        "error"=>"Wrong password",
-        "debug_db_pass"=>$user['password']
+        "status"=>"error",
+        "message"=>"Wrong password"
     ]);
 }
 ?>
